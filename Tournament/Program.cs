b5fc44226db596ae28fdb6e8d3a0cb17e45cf7b7
cameron joseph <cameronjoseph357@gmail.com>
2020-03-03 12:45:01 -0800

    project start

---
+++ Tournament/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printMessage = "Starting Tournament Tracking";
+            Console.WriteLine("Message: {0}", printMessage);
+        }
+    }
+}
---
+++ TournamentLibrary/TeamModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public  class TeamModel
+    {
+        // NOTE: get package that allow reference count to show above each function/property
+        public List<int> TeamMembers { get; set; } = new List<int>();
+        public string TeamName { get; set; }
+
+    }
+}
